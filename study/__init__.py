44562d4f3229b06652949d85c26cc2e1738d924e
floradrucker <flora.drucker@gmail.com>
2025-05-21 18:43:35 +0200

    set what is displayed when

--- study/__init__.py
+++ study/__init__.py
@@ -10,9 +10,9 @@ Task
 class C(BaseConstants):
     NAME_IN_URL = 'study'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 2  # = number of sessions including trial. Certain pages will only be shown in certain rounds, e.g. predicted and ideal in only rounds 2 and 6
+    NUM_ROUNDS = 6  # = number of sessions including trial. Certain pages will only be shown in certain rounds, e.g. predicted and ideal in only rounds 2 and 6
     USE_TIMEOUT = True
-    TIMEOUT_SECONDS = 120
+    TIMEOUT_SECONDS = 60  # TODO: set this to 600 for the real experiment (10 minutes)
     TIMEOUT_MINUTES = round(TIMEOUT_SECONDS / 60)
     TASK_LENGTH = 4
 
@@ -75,6 +75,10 @@ class Player(BasePlayer):
         blank=False,
         label="How many tasks do you ideally want to do for 150 cents per task?"
     )
+    lastideal = models.IntegerField(
+        blank=False,
+        label="How many tasks do you ideally want to do in this round for what you think the task payoff is?"
+    )
 
     # Predicted values
     predicted50 = models.IntegerField(
@@ -121,6 +125,10 @@ class Player(BasePlayer):
         blank=False,
         label="How many tasks do you predict you will do for 150 cents per task?"
     )
+    lastpredicted = models.IntegerField(
+        blank=False,
+        label="How many tasks do you predict you will do in this round for what you think the task payoff is?"
+    )
 
     belief = models.IntegerField(
         blank=False,
@@ -148,6 +156,8 @@ def creating_session(subsession: Subsession):
     dictionary = dict([(d[0][i], d[1][i]) for i in range(26)])
     subsession.dictionary = json.dumps(dictionary)
 
+    # TODO: select the 5 percent here?
+
 
 def live_update_performance(player: Player, data):
     own_id = player.id_in_group
@@ -165,6 +175,11 @@ def live_update_performance(player: Player, data):
 
 # PAGES
 class Interval(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == 2  # only shown in the first real round
+
+    @staticmethod
     def vars_for_template(player):
         return {
             'benefit_min': base_constants.BENEFIT_RANGE_MIN,
@@ -173,10 +188,15 @@ class Interval(Page):
 
 
 class Ideal(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == 2 or player.round_number == 6
+
     form_model = 'player'
     form_fields = ['ideal50', 'ideal60', 'ideal70', 'ideal80', 'ideal90', 'ideal100',
-                   'ideal110', 'ideal120', 'ideal130', 'ideal140', 'ideal150']
+                   'ideal110', 'ideal120', 'ideal130', 'ideal140', 'ideal150', 'lastideal']
 
+    @staticmethod
     def vars_for_template(player):
         return {
             'percent_ideal': base_constants.PERCENT_IDEAL,
@@ -184,23 +204,33 @@ class Ideal(Page):
 
 
 class Predicted(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number == 2 or player.round_number == 6
+
     form_model = 'player'
     form_fields = ['predicted50', 'predicted60', 'predicted70', 'predicted80', 'predicted90',
                    'predicted100', 'predicted110', 'predicted120', 'predicted130', 'predicted140',
-                   'predicted150']
+                   'predicted150', 'lastpredicted']
 
 
 class Belief(Page):
+    @staticmethod
+    def is_displayed(player):
+        return player.round_number > 1
+
     form_model = 'player'
     form_fields = ['belief']
 
 
 class Signal(Page):
-    pass
+    @staticmethod
+    def is_displayed(player):
+        return 1 < player.round_number < 6
 
 
 class Work(Page):  # in period 5, we tell the participants the number of tasks they have to do here
-    pass
+    pass  # TODO: only display this to the x percent in period 6 or in all periods but different text?
 
 
 class Task(Page):
@@ -211,7 +241,7 @@ class Task(Page):
         timeout_seconds = C.TIMEOUT_SECONDS
 
     @staticmethod
-    def vars_for_template(player: Player):
+    def vars_for_template(player):
         letters_per_word = C.TASK_LENGTH
         task_list = [j for j in range(letters_per_word)]
         legend_list = [j for j in range(26)]
@@ -223,11 +253,13 @@ class Task(Page):
 
 
 class Results(Page):
-    pass
+    pass  # this we don't need actually. or do we?
 
 
 class Survey(Page):
-    pass
+    @staticmethod
+    def is_displayed(player):
+        return 1 < player.round_number < 6
 
 
 page_sequence = [
